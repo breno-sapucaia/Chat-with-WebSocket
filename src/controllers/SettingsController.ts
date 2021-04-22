366f9350b9d23d0381397f67f396e123ed9815e8
Breno Sapucaia <brenoss.magalhaes@outlook.com>
2021-04-22 10:38:54 -0300

    aula 3 feita

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,23 +1,20 @@
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm';
 import SettingsRepository from '../repositories/SettingsRepository';
+import SettingsService from '../services/SettingsService';
 
 export default class SettingsController {
     async create(request: Request, response: Response) {
 
         const { chat, username } = request.body;
 
-        const settingsRepository = getCustomRepository(SettingsRepository)
-
-        const settings = settingsRepository.create({
-            chat,
-            username
-        })
-
-        await settingsRepository.save(settings)
-
-        return response.json(settings).status(200)
-
+        const settingsService = new SettingsService();
+        try{
+            const settings = settingsService.create({chat, username});
+            return response.json(settings).status(200);
+        }catch(err){
+            return response.status(400).json(err.message)
+        }
     }
 
 }
